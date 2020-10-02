7efdf28af0b2d5586b2a25c7e31754746f315ff3
Tech Kamar <techkamar@gmail.com>
2020-10-02 20:08:54 +0530

    Added SentenceGenerator class

--- tools/sentence_generator.py
+++ tools/sentence_generator.py
@@ -1,30 +1,58 @@
 import string 
-import random 
+import random
+import sys
+import json
 
-sentence_list=["how much for <item>?"
-,"what is the price of <item>?"
-,"how much do <item> cost?"
-,"price of <item>?"
-,"give me 5 kg of <item>."
-,"I need 1 packet of <item>."
-,"give 5 kilograms of <item>"
-,"I need 7.5 kilograms of <item>"
-,"give me kg 5 of <item>."
-,"I need packet 2 of <item>."
-,"give kilograms 6.25 of <item>"
-,"I need kilograms 7.5 of <item>"]
 
-def get_random_string():
-	N = random.randint(5,12)  
-	res = ''.join(random.choices(string.ascii_uppercase +
-                             	string.ascii_lowercase, k = N))
-	return res
+class SentenceGenerator:
+	def __init__(self):
+		try:
+			str(sys.argv[1])
+		except:
+			print("ERROR!!! Input Text File not specified")
+			exit(0)
 
+		try:
+			str(sys.argv[2])
+		except:
+			print("ERROR!!! Input JSON File not specified")
+			exit(0)
+
+		try:
+			str(sys.argv[3])
+		except:
+			print("ERROR!!! Output File not specified")
+			exit(0)
+
+		# Read the input file
+		self.input_patterns_list = self.get_content_of_file(sys.argv[1])
+
+		# Get the config to generate the sentences for labels
+		self.label_config_json = json.loads(self.get_content_of_file(sys.argv[2]))
+
+	def get_content_of_file(self,file_name):
+		content = ""
+		with open(file_name) as inp:
+			for line in inp:
+				line = line.strip()
+				content += line
+		return content
+
+	def get_random_string(self, min_length, max_length):
+		length = random.randint(min_length, max_length)
+		res = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k = length))
+		return res
+
+
+	def generate(self):
+		pass
+
+
+# MAIN METHOD or ENTRY point
 def main():
-	for sentence in sentence_list:
-		for i in range(0,10):
-			new_sent = sentence.replace("<item>",get_random_string())
-			print(new_sent)
+	generator = SentenceGenerator()
+	generator.generate()
+
 
 if __name__=="__main__":
 	main()
